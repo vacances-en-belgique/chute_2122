fc3cf559e8156ad9355afdf0fb44378aa212e780
vacances-en-belgique <128829377+vacances-en-belgique@users.noreply.github.com>
2023-03-24 17:44:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,247 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    $(this).toggleClass("text-selected");
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const fumée = document.getElementById('fumée');
+fumée.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+img.src = 'https://static.wixstatic.com/media/e9f423_22d54a105d35497590ac160644018909~mv2.jpg/v1/fill/w_574,h_574,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/WING%24%24%20OF%20LYBERTY.jpg';
+
+
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const botasolar = document.getElementById('botasolar');
+botasolar.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_dc422ea964a140b38d8136de9ee388a0~mv2.jpg/v1/fill/w_1960,h_1102,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/e9f423_dc422ea964a140b38d8136de9ee388a0~mv2.jpg';
+  
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const flawed = document.getElementById('flawed');
+flawed.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_4c5b3ebe21da4c279e80eea0cd2d50e7~mv2.jpg/v1/fill/w_666,h_1000,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/flawd.jpg';
+  
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const murmures = document.getElementById('murmures');
+murmures.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_3932720fecab407e853f83101d37ee41~mv2.jpg/v1/fill/w_1200,h_800,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/mur.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const luz = document.getElementById('luz');
+luz.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_ab20719999664264a5f7d5fb142c4488~mv2.jpg/v1/fill/w_1200,h_800,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_ab20719999664264a5f7d5fb142c4488~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const crane = document.getElementById('crane');
+crane.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_ebc0204dd5ae451592ff0e66172259cc~mv2.jpg/v1/fill/w_798,h_1200,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_ebc0204dd5ae451592ff0e66172259cc~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const medusa = document.getElementById('medusa');
+medusa.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_80bc0533ad984bfe90ac4f9d7651ed77~mv2.jpg/v1/fill/w_800,h_1200,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_80bc0533ad984bfe90ac4f9d7651ed77~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const chute = document.getElementById('chute');
+chute.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_23427ffb6c124bf1b6b54b847b8c4952~mv2.jpg/v1/fill/w_800,h_1200,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_23427ffb6c124bf1b6b54b847b8c4952~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const souriez = document.getElementById('souriez');
+souriez.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_62958293f8804438a777b0a61d4024a6~mv2.jpg/v1/fill/w_800,h_1200,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_62958293f8804438a777b0a61d4024a6~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const tunnel = document.getElementById('tunnel');
+tunnel.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_be1f446b04da47eead628b96e4f7ea59~mv2.jpg/v1/fill/w_1200,h_798,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_be1f446b04da47eead628b96e4f7ea59~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const bienvenue = document.getElementById('bienvenue');
+bienvenue.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_be4290344a094394ad1012a3d8cb4441~mv2.jpg/v1/fill/w_798,h_1200,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_be4290344a094394ad1012a3d8cb4441~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const moche = document.getElementById('moche');
+moche.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_e8f902a86d484d4aa1e6dccd568e03f0~mv2.jpg/v1/fill/w_1200,h_798,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_e8f902a86d484d4aa1e6dccd568e03f0~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const proprio = document.getElementById('proprio');
+proprio.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_06acb6f22f0c40f0bb140a715a91ff31~mv2.jpg/v1/fill/w_1200,h_798,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_06acb6f22f0c40f0bb140a715a91ff31~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
+
+// Récupération de l'élément li et ajout d'un événement au clic
+const vapeur = document.getElementById('vapeur');
+vapeur.addEventListener('click', () => {
+  // Affichage de la lightbox
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
+
+  // Récupération de l'image et mise à jour de son src
+  const img = lightbox.querySelector('img');
+  img.src = 'https://static.wixstatic.com/media/e9f423_6136b9fc0ab548fdb5ce0109bf34d5d3~mv2.jpg/v1/fill/w_1200,h_798,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e9f423_6136b9fc0ab548fdb5ce0109bf34d5d3~mv2.jpg';
+
+  // Fermeture de la lightbox au clic sur l'image
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+  });
+});
